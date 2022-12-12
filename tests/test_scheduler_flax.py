a9f7aedc6f1d1624ccebfc8f6690e8762e0e393b
Simon Kirsten <simonkirsten24@gmail.com>
2022-12-12 02:16:40 +0100

    Add missing set_timesteps in test

--- tests/test_scheduler_flax.py
+++ tests/test_scheduler_flax.py
@@ -873,10 +873,15 @@ class FlaxPNDMSchedulerTest(FlaxSchedulerCommonTest):
 
     def test_inference_plms_no_past_residuals(self):
         with self.assertRaises(ValueError):
+            sample, _ = self.dummy_sample
+            num_inference_steps = 10
+
             scheduler_class = self.scheduler_classes[0]
             scheduler_config = self.get_scheduler_config()
             scheduler = scheduler_class(**scheduler_config)
             state = scheduler.create_state()
+
+            state = scheduler.set_timesteps(state, num_inference_steps, shape=sample.shape)
 
             scheduler.step_plms(state, self.dummy_sample, 1, self.dummy_sample).prev_sample
 
